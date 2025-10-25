d14f0113f28a8d50b6d0d0111c00e2c8738c3012
OmarH-creator <omarhassanelsayedhassan@gmail.com>
2025-10-25 20:50:34 +0300

    initial Admin Dashboard interface (App.js problem)

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders admin dashboard heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/University Management — Admin/i);
+  expect(heading).toBeInTheDocument();
 });
