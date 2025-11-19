93ea4a3f6b33c218eb57b43235679415bd138f86
Mohamed Besheer <mbesheer26105@gmail.com>
2025-11-20 00:52:08 +0200

    added the database connection logic

--- Backend/backend/src/main/java/com/university/backend/BackendApplication.java
+++ Backend/backend/src/main/java/com/university/backend/BackendApplication.java
@@ -2,12 +2,27 @@ package com.university.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner testDbConnection(DataSource dataSource) {
+		return args -> {
+			try (Connection conn = dataSource.getConnection()) {
+				System.out.println("DB Connection SUCCESS! Catalog is: " + conn.getCatalog());
+			} catch (Exception ex) {
+				System.err.println("DB Connection FAILURE: " + ex.getMessage());
+			}
+		};
 	}
 
 }
