c6754595c7403ba005c1d2fde56400a9fa412b12
Seif <seifaly48@gmail.com>
2025-12-24 12:22:16 +0200

    added CoursePrerequisiteRepositoryrepo

---
+++ Backend/backend/src/main/java/com/university/backend/repository/CoursePrerequisiteRepository.java
@@ -0,0 +1,15 @@
+package com.university.backend.repository;
+
+import com.university.backend.entity.CoursePrerequisite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@org.springframework.transaction.annotation.Transactional
+public interface CoursePrerequisiteRepository extends JpaRepository<CoursePrerequisite, Long> {
+    List<CoursePrerequisite> findByCourse(com.university.backend.entity.Course course);
+    void deleteByCourse(com.university.backend.entity.Course course);
+    void deleteByPrerequisite(com.university.backend.entity.Course prerequisite);
+}
